9209ba14733a38fe38601cb51d791b1a6e7afec0
Andrew Hanrahan <andrew.hanrahan@gmail.com>
2024-07-20 04:28:22 -0500

    WIP: DIR TO page fixes
    DIR TO title color changes
    Waypoint, f-plan labels left aligned
    Removed * before [], removed [] around waypoint if entered
    dist, UTC alignment
    Save changes on DIR TO page label changed from "TMPY DIRECT" to "DIR TO INSERT"
    DIRECT TO color changes when waypoint is selected
    dist, UTC blank when waypoint selected
    waypoints in list will not appear selectable if the same waypoint is already selected
    
    WIP: radial in/out support

--- fbw-a32nx/src/base/flybywire-aircraft-a320-neo/html_ui/Pages/VCockpit/Instruments/Airliners/FlyByWire_A320_Neo/CDU/A320_Neo_CDU_DirectToPage.js
+++ fbw-a32nx/src/base/flybywire-aircraft-a320-neo/html_ui/Pages/VCockpit/Instruments/Airliners/FlyByWire_A320_Neo/CDU/A320_Neo_CDU_DirectToPage.js
@@ -3,19 +3,23 @@
 
 // TODO this whole thing is thales layout...
 
+const MODE_DIRECT = 1;
+const MODE_RADIAL_IN = 2;
+const MODE_RADIAL_OUT = 3;
+
 class CDUDirectToPage {
-    static ShowPage(mcdu, directWaypoint, wptsListIndex = 0) {
+    static ShowPage(mcdu, directWaypoint, wptsListIndex = 0, dirToMode = 1) {
         mcdu.clearDisplay();
         mcdu.page.Current = mcdu.page.DirectToPage;
         mcdu.returnPageCallback = () => {
-            CDUDirectToPage.ShowPage(mcdu, directWaypoint, wptsListIndex);
+            CDUDirectToPage.ShowPage(mcdu, directWaypoint, wptsListIndex, dirToMode);
         };
 
         mcdu.activeSystem = 'FMGC';
 
-        let directWaypointCell = "";
+        let directWaypointIdent = "";
         if (directWaypoint) {
-            directWaypointCell = directWaypoint.ident;
+            directWaypointIdent = directWaypoint.ident;
         } else if (mcdu.flightPlanService.hasTemporary) {
             mcdu.eraseTemporaryFlightPlan(() => {
                 CDUDirectToPage.ShowPage(mcdu);
@@ -33,8 +37,8 @@ class CDUDirectToPage {
             iMax--;
             eraseLabel = "\xa0DIR TO[color]amber";
             eraseLine = "{ERASE[color]amber";
-            insertLabel = "TMPY\xa0[color]amber";
-            insertLine = "DIRECT*[color]amber";
+            insertLabel = "DIR TO\xa0[color]amber";
+            insertLine = "INSERT*[color]amber";
             mcdu.onLeftInput[5] = () => {
                 mcdu.eraseTemporaryFlightPlan(() => {
                     CDUDirectToPage.ShowPage(mcdu);
@@ -79,14 +83,19 @@ class CDUDirectToPage {
             });
         };
 
+        mcdu.onRightInput[1] = () => {
+            CDUDirectToPage.ShowPage(mcdu, directWaypoint, wptsListIndex, 1);
+        };
         mcdu.onRightInput[2] = () => {
             mcdu.setScratchpadMessage(NXFictionalMessages.notYetImplemented);
         };
         mcdu.onRightInput[3] = () => {
             mcdu.setScratchpadMessage(NXFictionalMessages.notYetImplemented);
+            CDUDirectToPage.ShowPage(mcdu, directWaypoint, wptsListIndex, 2);
         };
         mcdu.onRightInput[4] = () => {
             mcdu.setScratchpadMessage(NXFictionalMessages.notYetImplemented);
+            CDUDirectToPage.ShowPage(mcdu, directWaypoint, wptsListIndex, 3);
         };
 
         const plan = mcdu.flightPlanService.active;
@@ -112,7 +121,8 @@ class CDUDirectToPage {
                     continue;
                 }
 
-                waypointsCell[cellIter] = "{" + leg.ident + "[color]cyan";
+                // Naive handling of detecting if waypoint is already selected
+                waypointsCell[cellIter] = (leg.ident === directWaypointIdent ? "\xa0" : "{") + leg.ident + "[color]cyan";
                 if (waypointsCell[cellIter]) {
                     mcdu.onLeftInput[cellIter + 1] = () => {
                         mcdu.eraseTemporaryFlightPlan(() => {
@@ -139,30 +149,43 @@ class CDUDirectToPage {
         if (wptsListIndex < totalWaypointsCount - 5) {
             mcdu.onUp = () => {
                 wptsListIndex++;
-                CDUDirectToPage.ShowPage(mcdu, directWaypoint, wptsListIndex);
+                CDUDirectToPage.ShowPage(mcdu, directWaypoint, wptsListIndex, dirToMode);
             };
             up = true;
         }
         if (wptsListIndex > 0) {
             mcdu.onDown = () => {
                 wptsListIndex--;
-                CDUDirectToPage.ShowPage(mcdu, directWaypoint, wptsListIndex);
+                CDUDirectToPage.ShowPage(mcdu, directWaypoint, wptsListIndex, dirToMode);
             };
             down = true;
         }
-        mcdu.setArrows(up, down, false ,false);
+        mcdu.setArrows(up, down, false, false);
+
+        // AJH
+        const hasTemporary = mcdu.flightPlanService.hasTemporary;
+        const colorForHasTemporary = hasTemporary ? "yellow" : "cyan";
+        const directWaypointCell = directWaypointIdent ? directWaypointIdent + "[color]yellow" : "[\xa0\xa0\xa0\xa0\xa0][color]cyan";
+        // TODO: calculate distance and UTC
+        const distanceCell = hasTemporary ? "\xa0\xa0\xa0[color]yellow" : "---";
+        const utcCell = hasTemporary ? "\xa0\xa0\xa0\xa0[color]yellow" : "----";
+        // TODO: support abeam
+        // TODO: calculate radial in/out, support setting radial in, radial out
+        const radialInCell = "[\xa0]°[color]" + (dirToMode === MODE_RADIAL_IN ? colorForHasTemporary : "cyan");
+        const radialOutCell = "[\xa0]°[color]" + (dirToMode === MODE_RADIAL_OUT ? colorForHasTemporary : "cyan");
+
         mcdu.setTemplate([
-            ["DIR TO"],
-            ["\xa0WAYPOINT", "DIST\xa0", "UTC"],
-            ["*[" + (directWaypointCell ? directWaypointCell : "\xa0\xa0\xa0\xa0\xa0") + "][color]cyan", "---", "----"],
-            ["\xa0F-PLN WPTS"],
-            [waypointsCell[0], "DIRECT TO[color]cyan"],
+            ["DIR TO[color]" + colorForHasTemporary],
+            ["WAYPOINT", "DIST", "UTC"],
+            [directWaypointCell, distanceCell, utcCell],
+            ["F-PLN WPTS"],
+            [waypointsCell[0], "DIRECT TO[color]" + (dirToMode === MODE_DIRECT ? colorForHasTemporary : "cyan")],
             ["", "WITH\xa0"],
             [waypointsCell[1], "ABEAM PTS[color]cyan"],
             ["", "RADIAL IN\xa0"],
-            [waypointsCell[2], "[ ]°[color]cyan"],
+            [waypointsCell[2], radialInCell],
             ["", "RADIAL OUT\xa0"],
-            [waypointsCell[3], "[ ]°[color]cyan"],
+            [waypointsCell[3], radialOutCell],
             [eraseLabel, insertLabel],
             [eraseLine ? eraseLine : waypointsCell[4], insertLine]
         ]);
